1628fde8bb86188d3926d88bfcc4b3f90fdeebe3
RohanSreelesh <rohansreelesh02@gmail.com>
2022-05-07 21:43:47 -0400

    removed if

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     println!("Welcome to the Rabbit University Safety Testing Suite (RUSTs)");
 
@@ -50,7 +49,8 @@ fn calculate_safety_status(
     has_carrot: bool,
     friends_nearby: i32,
 ) -> bool {
-    if (!wolves_nearby && day_time) || has_carrot || (friends_nearby > 3){true} else {false}}
+    (!wolves_nearby && day_time) || has_carrot || (friends_nearby > 3)
+}
 
 /// Validate the simulation
 fn validate_simulation() {
